1c25d094237f45bf34410642ea75c735472efbd3
Felipe da Silva Rodrigues <felipedasr3232@gmail.com>
2023-05-14 22:20:50 -0300

    Criação dos componentes do blog Menu, Titulo, Big e Small Cards

---
+++ src/app/components/smal-card/smal-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SmalCardComponent } from './smal-card.component';
+
+describe('SmalCardComponent', () => {
+  let component: SmalCardComponent;
+  let fixture: ComponentFixture<SmalCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SmalCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SmalCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
